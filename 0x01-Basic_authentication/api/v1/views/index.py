02070c35aed2fcde1512bec6455a69eadf4e838f
wikki2000 <wisdomokposin@gmail.com>
2024-11-13 08:34:43 +0100

    Implement the 401 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorize() -> str:
+    """ GET /api/v1/unauthorized
+
+    return:
+        - unauthorize error response
+    """
+    abort(401)
