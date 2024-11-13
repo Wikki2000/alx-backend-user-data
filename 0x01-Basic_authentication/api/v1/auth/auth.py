3c9164bebd89119be9f831229ec109a6d9a9809a
wikki2000 <wisdomokposin@gmail.com>
2024-11-13 12:01:43 +0100

    Add boilerplate of authentication module

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+
+
+class Auth:
+    """ Auth class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Check path that require auth """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Check authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Check current user """
+        return None
