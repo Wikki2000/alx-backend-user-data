dd1a57d31f7d57d8efdf7f08e93c05f85d024806
wikki2000 <wisdomokposin@gmail.com>
2024-11-15 18:09:52 +0100

    Implement method to decode base

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Baic Authentication Module """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -8,7 +9,8 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+            self, authorization_header: str
+    ) -> str:
         """ Extract authorization header
         """
         if not authorization_header:
@@ -18,3 +20,24 @@ class BasicAuth(Auth):
         elif not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """ Check if header is valid
+        """
+        if not base64_authorization_header:
+            return None
+        elif not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # Convert string to bytes and decode Base64.
+            # Can only use base64 to decode uft-8 encoded str.
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64.b64decode(base64_bytes)
+
+            return message_bytes.decode('utf-8')
+        except Exception as e:
+            print(str(e))
+            return None
